04533e65f63452f26e44376c5c8bf28a1ac64378
Juan Fernandez <jafern14@gmail.com>
2016-04-07 13:44:01 -0400

    Build.

--- dist/jquery-element-equalizer.js
+++ dist/jquery-element-equalizer.js
@@ -1,2 +1,20 @@
+(function ($) {
 
+    var pluginName = 'equalizer',
+        defaults = {
+        eleGroupCount: 1,
+        widthEqualizing: false,
+        heightEqualizing: true,
+        resizingRatio: 'auto'
+    };
+
+    $.fn[pluginName].defaults = defaults;
+
+    $.fn[pluginName] = function (options) {
+        var opts = $.extend({}, $.fn[pluginName].defaults, options);
+        console.log(opts);
+    };
+
+    $(window).on('DOMContentLoaded load resize', function () {});
+})(window.jQuery);
 //# sourceMappingURL=jquery-element-equalizer.js.map
